ece7276417af50ef82364df0466039c91171e5fa
Zachary Meredith <zack@Zacharys-MacBook-Pro-2.local>
2016-02-28 22:28:53 -0600

    typekit fixes

--- app.node.js
+++ app.node.js
@@ -417,12 +417,6 @@ module.exports =
 /* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
-  /**
-   * React Static Boilerplate
-   * https://github.com/koistya/react-static-boilerplate
-   * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
-   */
-
   'use strict';
 
   Object.defineProperty(exports, '__esModule', {
