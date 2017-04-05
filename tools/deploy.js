e44f2a7bf7128891def39caec43153a24cb4b71c
Zachary Meredith <zack@Zacharys-MacBook-Pro-4.local>
2017-04-05 08:57:33 -0500

    test

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,39 +1,34 @@
 import Repo from 'git-repository';
 require("babel-core/register");
 require('babel-polyfill');
+require("babel-polyfill");
+const path = require("path");
+import run from './run';
+const ghPages = require('gh-pages');
 
-// TODO: Update deployment URL
 const remote = {
   name: 'zack',
   url: 'https://github.com/zackmeredith/zack.git',
   branch: 'gh-pages'
 };
 
-/**
- * Deploy the contents of the `/build` folder to GitHub Pages.
- */
 async function deploy() {
-  // Initialize a new Git repository inside the `/build` folder
-  // if it doesn't exist yet
-  const repo = await Repo.open('./build', { init: true });
-  await repo.setRemote(remote.name, remote.url);
 
-  // Fetch the remote repository if it exists
-  if ((await repo.hasRef(remote.url, remote.branch))) {
-    await repo.fetch(remote.name);
-    await repo.reset(`${remote.name}/${remote.branch}`, { hard: true });
-    await repo.clean({ force: true });
-  }
+global.DEBUG = process.argv.includes('--debug') || false;
+ const publish = (dir) => new Promise((resolve, reject) => {
+   ghPages.publish(dir, {}, (err) => {
+     if (err) {
+       reject();
+     } else {
+       resolve();
+     }
+   });
+ });
 
-  // Build the project in RELEASE mode which
-  // generates optimized and minimized bundles
-  process.argv.push('release');
-  await require('./build');
-
-  // Push the contents of the build folder to the remote server via Git
-  await repo.add('--all .');
-  await repo.commit('Update ' + new Date().toISOString());
-  await repo.push(remote.name, 'master:' + remote.branch);
+ return Promise.resolve()
+    .then(() => run(require("./clean").default))
+    .then(() => run(require("./build").default))
+    .then(() => publish(path.join(__dirname, 'build')));
 }
 
 export default deploy;
